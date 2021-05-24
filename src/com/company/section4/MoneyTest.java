640c1b1f329dcaf0d534e89bab85270b620b66bc
Tomoyuki Mikami <mika0215@gmail.com>
2021-05-24 09:53:46 +0900

    complete section 4

---
+++ src/com/company/section4/MoneyTest.java
@@ -0,0 +1,19 @@
+package com.company.section4;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MoneyTest {
+    @Test
+    public void TestMultiplication() {
+        Dollar five = new Dollar(5);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
+    }
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+    }
+}
