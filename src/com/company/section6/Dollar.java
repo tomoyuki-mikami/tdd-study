b1efe3a5b0a8df31cffc64cd2398441a6f810d96
Tomoyuki Mikami <mika0215@gmail.com>
2021-05-24 10:17:49 +0900

    complete section 6

---
+++ src/com/company/section6/Dollar.java
@@ -0,0 +1,10 @@
+package com.company.section6;
+
+public class Dollar extends Money {
+    Dollar(int amount) {
+        this.amount = amount;
+    }
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+}
