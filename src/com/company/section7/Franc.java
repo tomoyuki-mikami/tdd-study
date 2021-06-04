c622562096e0a0cce7041b2891f807ebbfddf92a
Tomoyuki Mikami <mika0215@gmail.com>
2021-06-04 09:29:31 +0900

    complete section 7

---
+++ src/com/company/section7/Franc.java
@@ -0,0 +1,10 @@
+package com.company.section7;
+
+public class Franc extends Money {
+    Franc(int amount) {
+        this.amount = amount;
+    }
+    Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+}
