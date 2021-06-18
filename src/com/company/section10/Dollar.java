861c4eaa51e0abb85226d71b352c1ddff0c370ca
Tomoyuki Mikami <mika0215@gmail.com>
2021-06-18 09:57:42 +0900

    complete section 10

---
+++ src/com/company/section10/Dollar.java
@@ -0,0 +1,7 @@
+package com.company.section10;
+
+public class Dollar extends Money {
+    Dollar(int amount, String currency) {
+        super(amount, currency);
+    }
+}
---
+++ src/com/company/section10/Franc.java
@@ -0,0 +1,7 @@
+package com.company.section10;
+
+public class Franc extends Money {
+    Franc(int amount, String currency) {
+        super(amount, currency);
+    }
+}
